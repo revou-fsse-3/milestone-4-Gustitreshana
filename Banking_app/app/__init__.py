84fa21011622f57cab1c0a846030e99dc26fa678
Gustitreshana <gustith2105@gmail.com>
2024-03-13 01:13:53 +0700

    Add account management routes and load environment variables

--- Banking_app/app/__init__.py
+++ Banking_app/app/__init__.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from app.controllers.user_management_route import user_routes
+from app.controllers.account_management_route import account_routes
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['JWT_SECRET_KEY'] = os.getenv('super-secret-key')
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 jwt = JWTManager(app)
 
 app.register_blueprint(user_routes)
+app.register_blueprint(account_routes)
 
 @app.route('/')
 def my_app():
