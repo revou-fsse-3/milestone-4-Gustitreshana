df3c3bead6af31f065472625ab843514ab916ff9
Gustitreshana <gustith2105@gmail.com>
2024-03-13 22:05:12 +0700

    Add SQL script for creating tables in the Banking_app

---
+++ Banking_app/table.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE milestone_4;
+USE milestone_4;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    password_hash VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    account_type VARCHAR(255),
+    account_number VARCHAR(255) UNIQUE,
+    balance DECIMAL(10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    from_account_id INT,
+    FOREIGN KEY (from_account_id) REFERENCES accounts(id),
+    to_account_id INT,
+    FOREIGN KEY (to_account_id) REFERENCES accounts(id),
+    amount DECIMAL(10, 2),
+    type VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+SELECT *
+FROM users;
+
+SELECT *
+FROM accounts;
+
+SELECT *
+FROM transactions;
